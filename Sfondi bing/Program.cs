e568df0cbba926e066a2e2aa73c0b0f1f548fedc
Jack004 <jack004yt@gmail.com>
2017-08-08 16:19:29 +0200

    Aggiungere i file di progetto.

---
+++ Sfondi bing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace Sfondi_bing
+{
+    class Program
+    {
+        [DllImport("User32", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uiAction, int uiParam,
+        string pvParam, uint fWinIni);
+
+        static void Main(string[] args)
+        {
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load("https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=it-It"); //Load NOT LoadXml
+
+            XmlNodeList xmlnode = xml.GetElementsByTagName("urlBase");
+            string prova = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            WebClient w1 = new WebClient();
+            w1.DownloadFile("https://www.bing.com/" + xmlnode[0].InnerText + "_1920x1080.jpg", Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\potd.jpg");
+            //if false file not found
+            SystemParametersInfo(0x0014, 0, Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\potd.jpg", 0x0001);
+        }
+    }
+}
